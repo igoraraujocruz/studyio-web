26bfb1830211af7cac311a12a70a638e05ca2d8a
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-10-21 21:14:03 -0300

    refactor: signin style

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,15 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const apperFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
 
 export const Container = styled.div`
   height: 80vh;
@@ -6,6 +17,7 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  animation: ${apperFromLeft} 1s;
 
 
   form {
