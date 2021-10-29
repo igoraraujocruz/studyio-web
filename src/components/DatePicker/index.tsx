fc1ae39b030891c6bce8331010e59030c4e81ee9
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-10-29 15:58:48 -0300

    refactor(n): date picker
    n

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -21,10 +21,13 @@ export function DatePicker({ name, ...rest }: Props) {
     registerField({
       name: fieldName,
       ref: datepickerRef.current,
-      path: 'props.selected',
       clearValue: (ref: any) => {
         ref.clear();
       },
+      setValue: (e, v) => {
+        setDate(new Date(v));
+      },
+      getValue: () => datepickerRef.current.props.selected,
     });
   }, [fieldName, registerField]);
 
